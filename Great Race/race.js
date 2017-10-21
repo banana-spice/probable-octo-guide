adc40a56d3274ee2bba5c1e95c7b3cf4331cae6f
Vicky <banana.spice@yahoo.com>
2017-10-20 21:20:43 -0500

    it almost resets now

--- Great Race/race.js
+++ Great Race/race.js
@@ -7,31 +7,33 @@ var runDis2 = 0;
 
 function startRace(){
 	document.getElementById('stoplight').src = 'images/start.png';
-	runMeter = setInterval(runTime, 2500);
+	runMeter = setInterval(runTime, 2000);
 }
 
 function reset(){
 	var runDis1 = 0;
 	var runDis2 = 0;
-	margin1 = 0;
-	margin2 = 0;
-	document.getElementById("run2").style.marginLeft = margin2;
-	document.getElementById("run1").style.marginLeft = margin1;
+	
+	var randNum1 = 0;
+	var randNum2 = 0;
+	
+	document.getElementById("run1").style.marginLeft = "0px";
+	document.getElementById("run2").style.marginLeft = "0px";
 	
 	document.getElementById("run1").src = "images/kimirun.png";
 	document.getElementById("run2").src = "images/omirun.png";	
 	document.getElementById("stoplight").src = "images/stop.png";
-	document.getElementById("winner").innerhtml = " ";
+	document.getElementById("winner").innerHTML = " ";
 }
 
 function runTime(){
 	
-	randNum1 = Math.ceil(Math.random() * 150);
+	randNum1 = Math.ceil(Math.random() * 100);
 	runDis1 += randNum1;
 	var margin1 = runDis1 + "px";
 	document.getElementById("run1").style.marginLeft = margin1;
 	
-	randNum2 = Math.ceil(Math.random() * 150);
+	randNum2 = Math.ceil(Math.random() * 100);
 	runDis2 += randNum2;
 	var margin2 = runDis2 + "px";
 	document.getElementById("run2").style.marginLeft = margin2;
@@ -40,19 +42,19 @@ function runTime(){
 		clearInterval(runMeter);
 		document.getElementById("run2").src = "images/omipose.png";
 		document.getElementById("run1").src = "images/kimipose.png";
-		document.getElementById("winner").innerhtml = "<h2 onclick='reset();' >It's a Tie!</h2>";
+		document.getElementById("winner").innerHTML = "<h2>It's a Tie!</h2> <img src='images/kimipose.png' onclick='reset();'>";
 	
 	} else {
 		if (runDis1 >= 300){
 			clearInterval(runMeter);
 			document.getElementById("run1").src = "images/kimipose.png";
-			document.getElementById("winner").innerhtml = "<h2>Kimiko Wins!</h2> <img src='images/kimipose.png' onclick='reset();'>";
+			document.getElementById("winner").innerHTML = "<h2>Kimiko Wins!</h2> <img src='images/kimipose.png' onclick='reset();'>";
 		}
 		
 		if (runDis2 >= 300){
 			clearInterval(runMeter);
 			document.getElementById("run2").src = "images/omipose.png";
-			document.getElementById("winner").innerhtml = "<h2>Omi Wins!</h2> <img src='images/omipose.png' onclick='reset();'>";
+			document.getElementById("winner").innerHTML = "<h2>Omi Wins!</h2> <img src='images/omipose.png' onclick='reset();'>";
 		}
 	}
 }
