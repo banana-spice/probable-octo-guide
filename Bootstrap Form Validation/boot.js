55920ebd6e58a7da87846cd3897364289056569c
Vicky <banana.spice@yahoo.com>
2017-10-21 21:34:27 -0500

    stuck again

--- Bootstrap Form Validation/boot.js
+++ Bootstrap Form Validation/boot.js
@@ -11,24 +11,23 @@ function userName(){
 		document.getElementById("nameErr").style.display = "none"; 
 		document.getElementById("userName").style.backgroundColor = "white";
 		var nameVal = document.bootval.userName.value;
-		var goodNam = true;
+		return nameVal;
 	}
-	return false;
 }
 
-function checkMon(){
-	var checked = 0;
-    var len = document.bootval.pickMon.length;
-
-	for(var i = 0; i < len; i++) {
-		if(document.bootval.pickMon[i].checked){
-			var checked = 1;
-			var monVal = document.bootval.pickMon.value;
-			var goodMon = true;
-		}
-	}
-	if(checked == 0){
-		alert("A Monster must be selected")
+function emailCheck(){
+	var eVal = document.bootval.userEmail.value;
+	var patt = new RegExp (/\S+@\S+(\.\w{2,})+$/);
+	var res = patt.test(eVal);
+	
+	if (res == false) {
+		document.getElementById("emailErr").style.display = "block"; 
+		document.getElementById("userEmail").style.backgroundColor = "pink";
+	} else{
+		document.getElementById("emailErr").style.display = "none"; 
+		document.getElementById("userEmail").style.backgroundColor = "white";
+		var emailVal = document.bootval.userEmail.value;
+		return emailVal;
 	}
 }
 
@@ -44,41 +43,44 @@ function checkPhone(){
 		document.getElementById("phoneErr").style.display = "none"; 
 		document.getElementById("userPhone").style.backgroundColor = "white";
 		var phoneVal = document.bootval.userPhone.value;
-		var goodPhone = true;
+		return phoneVal;
 	}
 }
 
+function checkMon(){
+	var checked = 0;
+    var len = document.bootval.pickMon.length;
 
-function emailCheck(){
-	var eVal = document.bootval.userEmail.value;
-	var patt = new RegExp (/\S+@\S+(\.\w{2,})+$/);
-	var res = patt.test(eVal);
-	
-	if (res == false) {
-		document.getElementById("emailErr").style.display = "block"; 
-		document.getElementById("userEmail").style.backgroundColor = "pink";
-	} else{
-		document.getElementById("emailErr").style.display = "none"; 
-		document.getElementById("userEmail").style.backgroundColor = "white";
-		var emailVal = document.bootval.userEmail.value;
-		var goodEmail = true;
-	}
-}
-
-function check(){
-	userName();
-	emailCheck();
-	checkPhone();
-	checkMon();
-	
-	if (goodNam = true){
-		if (goodEmail = true){
-			if (goodPhone = true){
-				if (goodMon = true){
-					
-				}
-			}
+	for(var i = 0; i < len; i++) {
+		if(document.bootval.pickMon[i].checked){
+			var checked = 1;
+			
 		}
 	}
-	
+	if(checked == 0){
+		alert("A Monster must be selected")
+	} else {
+		var monVal = document.bootval.pickMon.value;
+		return monVal;
+	}
 }
+
+
+
+function check(){
+	var nameVal = userName();
+	var emailVal = emailCheck();
+	var phoneVal = checkPhone();
+	var monVal = checkMon();	
+	var hauntVal = document.bootval.whatHaunt.value;
+	
+	window.location.assign("thanks.html");
+}
+
+function fillThanks() {
+	
+	document.getElementById("formFill").innerHTML = "<p>Name: " + nameVal + "</p> <p>Email: " + emailVal + "</p> <p>Phone #: " + phoneVal + "</p> <p>Chosen Monster: " + monVal + "</p> <p>Favored Haunt: " + hauntVal + "</p>";
+}
+
+
+
